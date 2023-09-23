842669ca19f91ed92c843d4f09f723afe0de83de
charlesoller <charlesrello@gmail.com>
2023-09-23 16:29:55 -0400

    17 done

--- problems/17-bonus-permutations.js
+++ problems/17-bonus-permutations.js
@@ -13,10 +13,69 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
                         // [2, 1, 3], [2, 3, 1],
                         // [3, 1, 2], [3, 2, 1]]
 ***********************************************************************/
+function factorial(num) {
 
-const permutations = (array) => {
-  // Your code here 
+  if(num === 1) return num;
+
+
+  return (num * factorial(num - 1));
+
+  // return num === 1 ? num : num * factorial(num-1);
+}
+
+const permutations = (array, permArr = []) => {
+  // if(permArr.length === factorial(array.length)){
+  //   return permArr;
+  // }
+
+  // let temp = [];
+  // temp.push(array[0]);
+  // for(let i = 1; i < array.length; i++){
+  //   temp.push(array[i]);
+  // }
+  // permArr.push(temp);
+
+  // temp = [];
+  // temp.push(array[0]);
+  // for(let i = array.length-1; i > 0; i--){
+  //   if(array.length === 2){
+  //     temp.unshift(array[i]);
+  //   } else {
+  //     temp.push(array[i]);
+  //   }
+  // }
+  // permArr.push(temp);
+  // array.push(array.shift());
+
+  // return permutations(array, permArr);
+
+
+
+  if(permArr.length === factorial(array.length)){
+    return permArr;
+  }
+
+  let copy = array.slice();
+  for(let i = 0; i < array.length; i++){
+    for (let j = i+1; j < array.length; j++){
+      let firstEl = copy.slice(i, j);
+      copy.push(firstEl)
+      i++;
+    }
+  }
+
+
+  return permutations(array, permArr);
 };
+
+
+console.log(permutations([1, 2])) // [[1, 2], [2, 1]]
+console.log(permutations([1, 2, 3])) // [[1, 2, 3], [1, 3, 2],
+                        // [2, 1, 3], [2, 3, 1],
+                        // [3, 1, 2], [3, 2, 1]]
+
+// console.log(permutations([1, 2, 3, 4])) // [[1, 2, 3], [1, 3, 2],
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
