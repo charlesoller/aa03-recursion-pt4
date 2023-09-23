a47db8d8a67d8204e407d656cd66241eed78cf23
charlesoller <charlesrello@gmail.com>
2023-09-23 12:55:21 -0400

    2 Done

--- problems/14-deep-dup.js
+++ problems/14-deep-dup.js
@@ -35,9 +35,21 @@ console.log(x[0] === y[0]) // true
 
 
 function deepDup(arr) {
-  // Your code here 
+  if(typeof arr !== "object") return arr;
+
+  let copy = [];
+  for(let i = 0; i < arr.length; i++){
+    copy.push(deepDup(arr[i]));
+  }
+
+  return copy;
 }
 
+let arr = [[1], [2, [3]]];
+duped = deepDup(arr); // [[1], [2, [3]]]
+console.log(arr[0] === duped[0]) // false
+console.log(arr[1] === duped[1]) // false
+console.log(arr[1][1] === duped[1][1]) // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
